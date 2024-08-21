85e10a7ee650b4787bc7a8a54edea4e5ec8aedf5
go2665 <77224762+go2665@users.noreply.github.com>
2024-08-21 10:49:21 +0900

    03_3D_Basic - 문 작성 중
    자동문(미닫이, 여닫이, 일방향), 수동문 작성 중

--- 03_3D_Basic/Assets/Scripts/Door/DoorAuto.cs
+++ 03_3D_Basic/Assets/Scripts/Door/DoorAuto.cs
@@ -4,4 +4,19 @@ using UnityEngine;
 
 public class DoorAuto : DoorBase
 {
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Open(); // 플레이어가 발판위에 올라오면 문이 열린다.
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Close(); // 발판에서 플레이어가 나가면 문이 닫힌다.
+        }
+    }
 }
---
+++ 03_3D_Basic/Assets/Scripts/Test/Test06_Doors.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test06_Doors : MonoBehaviour
+{
+    // 문 두종류 만들기
+    // 1. SlidingDoor - 옆으로 열리는 자동문
+    // 2. OneWayDoor - 플레이어가 문앞에 있을 때만 열리는 문(뒤쪽은 열리지 않음, 코드로 구현하기)
+}
