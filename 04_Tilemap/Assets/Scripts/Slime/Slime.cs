312e76b108bc722188bc2917a38a08037970ec8d
go2665 <77224762+go2665@users.noreply.github.com>
2024-09-19 09:51:08 +0900

    04_Tilemap - 슬라임 팩토리 추가, 슬라임 셰이더 컨트롤 중

---
+++ 04_Tilemap/Assets/Scripts/Slime/Slime.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : RecycleObject
+{
+    /// <summary>
+    /// 페이즈 진행 시간(등장 연출 시간)
+    /// </summary>
+    public float phaseDuration = 0.5f;
+
+    /// <summary>
+    /// 디졸브 진행시간(사망 연출 시간)
+    /// </summary>
+    public float dissolveDuration = 1.0f;
+
+    /// <summary>
+    /// 아웃라인 기본 두깨
+    /// </summary>
+    public float VisibleOutlineThickness = 0.5f;
+
+    /// <summary>
+    /// 페이즈의 기본 두깨
+    /// </summary>
+    public float VisiblePhaseThickness = 0.1f;
+
+
+    /// <summary>
+    /// 머티리얼
+    /// </summary>
+    Material mainMaterial;
+
+    // 쉐이더 프로퍼티용 ID들
+    readonly int OutlineThicknessID = Shader.PropertyToID("_OutlineThickness");
+    readonly int PhaseSplitID = Shader.PropertyToID("_PhaseSplit");
+    readonly int PhaseThicknessID = Shader.PropertyToID("_PhaseThickness");
+    readonly int DissolveFadeID = Shader.PropertyToID("_DissolveFade");
+
+
+    // 슬라임은 풀로 관리된다. 팩토리를 이용해 생성할 수 있다.
+
+    private void Awake()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        mainMaterial = spriteRenderer.material;
+        
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        // 스폰될 때 Phase 작동
+        StartCoroutine(StartPhase());
+    }
+
+    protected override void OnReset()
+    {
+        ShowOutline(false);     // 아웃라인 끄기
+        mainMaterial.SetFloat(PhaseThicknessID, VisiblePhaseThickness); // 페이즈 두깨 원상복구 시키기
+        mainMaterial.SetFloat(PhaseSplitID, 1);         // 페이즈 시작 값으로 설정
+        mainMaterial.SetFloat(DissolveFadeID, 1);       // 디졸브 시작 값으로 설정
+    }
+
+    IEnumerator StartPhase()
+    {
+        // PhaseSplitID를 1 -> 0으로 만들기
+
+        float phaseNormalize = 1.0f / phaseDuration;    // 나누기 연산을 줄이기 위해 미리 계산
+        float timeElapsed = 0.0f;                       // 시간 누적용
+
+        while (timeElapsed < phaseDuration)             // 시간 될때까지 반복
+        {
+            timeElapsed += Time.deltaTime;              // 시간 누적
+            
+            //mainMaterial.SetFloat(PhaseSplitID, 1 - (timeElapsed/phaseDuration));
+            mainMaterial.SetFloat(PhaseSplitID, 1 - (timeElapsed * phaseNormalize));    // split값을 1 -> 0으로 점점 감소시키기
+            
+            yield return null;
+        }
+
+        mainMaterial.SetFloat(PhaseThicknessID, 0);     // 페이즈 선 안보이게 만들기
+        mainMaterial.SetFloat(PhaseSplitID, 0);         // 숫자 0으로 정리하기
+    }
+
+
+    public void Die()
+    {
+        // 죽을 때 Dissolve 작동
+    }
+
+    /// <summary>
+    /// 아웃라인을 보여줄지 말지 결정하는 함수
+    /// </summary>
+    /// <param name="isShow">true면 보여주고 false면 안보여준다.</param>
+    public void ShowOutline(bool isShow = true)
+    {
+        // isShow가 true면 VisibleOutlineThickness, 아니면 0으로 세팅
+        mainMaterial.SetFloat(OutlineThicknessID, isShow ? VisibleOutlineThickness : 0);  
+    }
+}
---
+++ 04_Tilemap/Assets/Scripts/Test/Test06_SlimePool.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test06_SlimePool : TestBase
+{
+    protected override void OnTest1(InputAction.CallbackContext context)
+    {
+        // 팩토리 이용해서 슬라임을 하나 꺼내기((-8,-4)~(8,4) 영역 안에 랜덤하게) 
+        Factory.Instance.GetSlime(new(Random.Range(-8, 8), Random.Range(-4, 4)));
+    }
+
+    protected override void OnTest2(InputAction.CallbackContext context)
+    {
+        // 씬에 있는 모든 슬라임의 아웃라인이 보인다.
+
+        Slime[] slimes = FindObjectsByType<Slime>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+        foreach (Slime slime in slimes)
+        {
+            slime.ShowOutline();
+        }
+    }
+
+    protected override void OnTest3(InputAction.CallbackContext context)
+    {
+        // 씬에 있는 모든 슬라임의 아웃라인이 안보인다.
+        Slime[] slimes = FindObjectsByType<Slime>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+        foreach (Slime slime in slimes)
+        {
+            slime.ShowOutline(false);
+        }
+    }
+
+    protected override void OnTest4(InputAction.CallbackContext context)
+    {
+        // 씬에 있는 모든 슬라임이 죽는다.
+    }
+}
