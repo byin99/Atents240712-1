2b1e8e4730884ddb5a5e88d5ecdd2f624c4f9be9
go2665 <77224762+go2665@users.noreply.github.com>
2024-09-26 11:44:18 +0900

    04_Tilemap - 서브맵 매니저 작성 중

--- 04_Tilemap/Assets/Scripts/Managers/GameManager.cs
+++ 04_Tilemap/Assets/Scripts/Managers/GameManager.cs
@@ -10,8 +10,21 @@ public class GameManager : Singleton<GameManager>
     public bool showSlimePath = false;
     public bool ShowSlimePath => showSlimePath;
 
+    SubmapManager submapManager;
+    public SubmapManager SubmapManager => submapManager;
+
+
+    protected override void OnPreInitialize()
+    {
+        base.OnPreInitialize();
+        submapManager = GetComponent<SubmapManager>();
+        submapManager.PreInitialize();
+    }
+
     protected override void OnInitialize()
     {
         player = FindAnyObjectByType<Player>();
+
+        submapManager.Initialize();
     }
 }
