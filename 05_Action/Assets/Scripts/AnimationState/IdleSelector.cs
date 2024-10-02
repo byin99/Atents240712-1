a0722ed76b15c5c6271c7b3a124a781964fa0f31
go2665 <77224762+go2665@users.noreply.github.com>
2024-10-02 12:53:27 +0900

    05_Action - 랜덤 Idle 동작 만들기 완료

--- 05_Action/Assets/Scripts/AnimationState/IdleSelector.cs
+++ 05_Action/Assets/Scripts/AnimationState/IdleSelector.cs
@@ -22,10 +22,35 @@ public class IdleSelector : StateMachineBehaviour
         int select = 0;
 
         // 이전 선택이 0번일 경우에만 일정확률로 1~4를 선택
+        if (prevSelect == 0)
+        {
+            float num = Random.value;
+
+            if( num < 0.01f )
+            {
+                select = 4;         // 1%
+            }
+            else if(num < 0.02f)
+            {
+                select = 3;         // 1%
+            }
+            else if (num < 0.03f)
+            {
+                select = 2;         // 1%
+            }
+            else if (num < 0.04f)
+            {
+                select = 1;         // 1%
+            }
+        }
+
         // testSelect가 Not_Select가 아닌 경우 무조건 설정된 값으로 변경(0~4만 가능)
+        if (testSelect != Not_Select)
+        {
+            select = Mathf.Clamp(testSelect, 0, 4);
+        }
 
-
-
+        prevSelect = select;        // 이전 선택을 기록
         return select;
     }
 }
