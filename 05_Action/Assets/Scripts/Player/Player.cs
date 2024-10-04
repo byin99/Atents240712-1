3cea842844e96eb8db58ca76b9f6b6002f346e83
go2665 <77224762+go2665@users.noreply.github.com>
2024-10-04 11:24:19 +0900

    05_Action - 플레이어 이동 완료, 이동 모드 변환 진행 중

--- 05_Action/Assets/Scripts/Player/Player.cs
+++ 05_Action/Assets/Scripts/Player/Player.cs
@@ -3,16 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerInputController), typeof(PlayerMovement))]
 public class Player : MonoBehaviour
 {
-    /// <summary>
-    /// 입력된 이동 방향(3D 공간의 이동 방향, y는 무조건 바닥 높이)
-    /// </summary>
-    Vector3 inputDirection  = Vector3.zero;
-
     // 컴포넌트 들
     CharacterController characterController;
+
     PlayerInputController inputController;
+    PlayerMovement movement;
 
 
     private void Awake()
@@ -20,29 +18,10 @@ public class Player : MonoBehaviour
         characterController = GetComponent<CharacterController>();
 
         inputController = GetComponent<PlayerInputController>();
-        inputController.onMove += OnMoveInput;
-        inputController.onMoveModeChange += OnMoveModeChangeInput;
-    }
+        movement = GetComponent<PlayerMovement>();
 
-    private void Update()
-    {
-        characterController.Move(Time.deltaTime * inputDirection);  // 수동
-        //characterController.SimpleMove(inputDirection);             // 자동
-    }
+        inputController.onMove += movement.SetDirection;
+        inputController.onMoveModeChange += movement.ToggleMoveMode;
 
-    /// <summary>
-    /// 이동입력에 대한 델리게이트로 실행되는 함수
-    /// </summary>
-    /// <param name="input">입력된 방향</param>
-    /// <param name="isPress">키를 눌렀으면 true, 땠으면 false</param>
-    private void OnMoveInput(Vector2 input, bool isPress)
-    {
-        // inputDirection 설정
-        // 입력에 따라 카메라 기준으로 앞뒤좌우로 이동한다.
-    }
-
-    private void OnMoveModeChangeInput()
-    {
-        
     }
 }
